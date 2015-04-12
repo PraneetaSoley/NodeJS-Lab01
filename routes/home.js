0fad10cf531925ac9088d898ce8caa79f9761250
Praneeta Soley <praneetasoley@gmail.com>
2015-04-12 16:37:36 -0700

    First Comit

---
+++ routes/home.js
@@ -0,0 +1,108 @@
+var ejs = require("ejs");
+var user = require('./user');
+
+exports.home = function(req, res){
+	
+	console.log("loading home page");
+	if(req.session !== null || req.session !== undefined)
+	{
+		if(req.session.user_id === null || req.session.user_id === undefined)
+		{
+			req.session.destroy(function(err){
+				if(err)
+				{
+					console.log(err);
+				}
+			});
+			res.render('signin', { title: 'Express' });
+		}
+		else
+		{
+			console.log(req.session);
+			res.render('home', { title:'Express', session:req.session});
+		}
+	}
+	else
+	{
+		res.render('signin', { title: 'Express' });
+	}
+};
+
+exports.addConnections = function(req, res){
+	  
+	if(req.session !== null || req.session !== undefined)
+	{
+		if(req.session.user_id === null || req.session.user_id === undefined)
+		{
+			req.session.destroy(function(err){
+				if(err)
+				{
+					console.log(err);
+				}
+			});
+			res.render('signin', { title: 'Express' });
+		}
+		else
+		{
+			res.render('addConnections', { title: 'Express' });
+			//res.render('home', { title:'Express', session:req.session});
+		}
+	}
+	else
+	{
+		res.render('signin', { title: 'Express' });
+	}
+};
+
+exports.searchResults = function(req, res){
+	  
+	if(req.session !== null || req.session !== undefined)
+	{
+		if(req.session.user_id === null || req.session.user_id === undefined)
+		{
+			req.session.destroy(function(err){
+				if(err)
+				{
+					console.log(err);
+				}
+			});
+			res.render('signin', { title: 'Express' });
+		}
+		else
+		{
+			//console.log(req.session);
+			res.render('searchResults', { title: 'SearchResults' , session:req.session});
+		}
+	}
+	else
+	{
+		res.render('signin', { title: 'Express' });
+	}
+};
+
+exports.displayProfile = function(req, res){
+	
+	console.log("loading guest profile page");
+	if(req.session !== null || req.session !== undefined)
+	{
+		if(req.session.user_id === null || req.session.user_id === undefined)
+		{
+			req.session.destroy(function(err){
+				if(err)
+				{
+					console.log(err);
+				}
+			});
+			res.render('signin', { title: 'Express' });
+		}
+		else
+		{
+			console.log(req.session);
+			res.render('displayProf', { title:'Express', session:req.session});
+		}
+	}
+	else
+	{
+		res.render('signin', { title: 'Express' });
+	}
+};
